891e080b556cef1b16bc56cb1d575ad8fb5ba16e
Aabir <aabirc390@gmail.com>
2021-10-09 04:15:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,121 @@
+var nav = document.querySelector('nav');
+window.addEventListener('scroll',function(){
+  if(window.pageYOffset>800){
+    nav.classList.add('bg-dark');
+  }else{
+   nav.classList.remove('bg-dark');
+  }
+});
+
+$(document).ready(function(){
+  $('.card1').hover(
+    function(){
+      $(this).animate({
+        marginTop: "-=1%",
+      },100);
+    },
+    function(){
+      $(this).animate({
+        marginTop: "0%"
+      },100);
+    }
+  );
+})
+$(document).ready(function(){
+  $('.card2').hover(
+    function(){
+      $(this).animate({
+        marginTop: "-=1%",
+      },100);
+    },
+    function(){
+      $(this).animate({
+        marginTop: "0%"
+      },100);
+    }
+  );
+})
+$(document).ready(function(){
+  $('.card3').hover(
+    function(){
+      $(this).animate({
+        marginTop: "-=1%",
+      },100);
+    },
+    function(){
+      $(this).animate({
+        marginTop: "0%"
+      },100);
+    }
+  );
+})
+$(document).ready(function(){
+  $('.card4').hover(
+    function(){
+      $(this).animate({
+        marginTop: "-=1%",
+      },100);
+    },
+    function(){
+      $(this).animate({
+        marginTop: "0%"
+      },100);
+    }
+  );
+})
+$(document).ready(function(){
+  $('.card5').hover(
+    function(){
+      $(this).animate({
+        marginTop: "-=1%",
+      },100);
+    },
+    function(){
+      $(this).animate({
+        marginTop: "0%"
+      },100);
+    }
+  );
+})
+$(document).ready(function(){
+  $('.card6').hover(
+    function(){
+      $(this).animate({
+        marginTop: "-=1%",
+      },100);
+    },
+    function(){
+      $(this).animate({
+        marginTop: "0%"
+      },100);
+    }
+  );
+})
+$(document).ready(function(){
+  $('.card7').hover(
+    function(){
+      $(this).animate({
+        marginTop: "-=1%",
+      },100);
+    },
+    function(){
+      $(this).animate({
+        marginTop: "0%"
+      },100);
+    }
+  );
+})
+$(document).ready(function(){
+  $('.card8').hover(
+    function(){
+      $(this).animate({
+        marginTop: "-=1%",
+      },100);
+    },
+    function(){
+      $(this).animate({
+        marginTop: "0%"
+      },100);
+    }
+  );
+})
